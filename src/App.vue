fc9010582aa5f7401c974eb3a4cf9e05555b07f9
Nass2912 <soobratty.as@gmail.com>
2025-06-26 12:08:51 +0200

    change name

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 				<div class="flex justify-between items-center">
 					<router-link to="/" class="flex items-center">
 						<img src="@/assets/logo.png" alt="Logo" class="h-12 mr-3" />
-						<span class="text-xl font-semibold text-[#46b7ff]">Grow Your Career</span>
+						<span class="text-xl font-semibold text-[#46b7ff]">TalentQuest</span>
 					</router-link>
 					<div class="space-x-4">
 						<!-- Conditionally render login/signup or logout -->
@@ -37,7 +37,7 @@
 
 		<footer class="bg-white shadow mt-8">
 			<div class="container mx-auto px-6 py-3">
-				<p class="text-center text-gray-600">© {{ new Date().getFullYear() }} Grow Your Career. All rights reserved.</p>
+				<p class="text-center text-gray-600">© {{ new Date().getFullYear() }} TalentQuest. All rights reserved.</p>
 			</div>
 		</footer>
 	</div>
